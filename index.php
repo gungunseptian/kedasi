29820fcc6f297df7c6dd3bfebea11aae415ed7b6
gungunseptian <itsmegoens@gmail.com>
2017-11-09 00:27:14 +0700

    update

---
+++ index.php
@@ -0,0 +1,178 @@
+<?php include('header.php'); ?>
+
+        <section class="main-slider">
+            <div class="flexslider">
+                <ul class="slides">
+                    <li style="background: url(img/kedasi/Header-Home-Page.jpg)">
+                        <div class="slider-text">
+                            <div class="inner">
+                                <!-- <div class="container text-center">
+                                    <div class="row">
+                                        <div class="col-md-8 col-md-offset-2">
+                                            <h1>Welcome to narani</h1>
+                                            <p>Quisque ligulas ipsum, euismod atras vulputate iltricies etri elit. Lorem ipsum dolor sit amet, consectetur.</p>
+                                            <a href="#" class="btn btn-lg border-white">Learn More</a>
+                                        </div>
+                                    </div>
+                                </div> -->
+                            </div>
+                        </div> 
+                    </li>
+                </ul>
+            </div>
+        </section>
+            <div class="divide40"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12" style="text-align: center;">
+                        <h3>Live Your Dream</h3> 
+                        <h4>
+                            Beautifully crafted, fully furnished Coworking & Office Suites
+                        </h4>
+                        <h4>for entrepreneurs, startups and businesses.</h4>
+                        <p><a href="#" class="btn btn-theme-bg">About Us</a></p><hr>
+                    </div>
+                </div>
+            </div>
+
+            <div class="divide40"></div>
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6" style="text-align: center;">
+                        <img src="img/work-1.png" class="img-responsive" alt="workimg">
+                        <div class="divide40"></div>
+                        <h3>Workspace is Changing</h3> 
+                        <p>Recruiting & retaining talent is harder than ever.</p>
+                        <p>Expectations are increasing. Our inviting, comfortable & uniquely designed spaces help you attract the best people, no matter your team size.</p>
+                    </div>
+
+                    <div class="col-md-6" style="text-align: center;">
+                        <img src="img/work-1.png" class="img-responsive" alt="workimg">
+                        <div class="divide40"></div>
+                        <h3>This is our life's work</h3> 
+                        <p>Do what you do best (your business), we’ll do the rest.</p>
+                        <p>No time lost negotiating leases, working with architects, buying furniture or renovating. Walk in, plug in and go. Grow when you’re ready!</p>
+                    </div>
+
+                </div>
+                <hr>
+            </div>
+
+            <div class="divide40"></div>
+             <div class="container">
+                <div class="row">
+                    <div class="col-md-12" style="text-align: center;">
+                        <h3>So much More than just a place to work.</h3> 
+                        <p>
+                            We create an environment of creativity & community, recognizing that
+collaboration between business owners (and employees) of differing organizations
+delivers greater growth, success and emotional satisfaction.
+                        </p>
+                        
+                    </div>
+
+                    <div class="col-md-12 margin20">
+                   
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe src="https://www.youtube.com/embed/ykYZdn1Uzu0"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            </div>
+
+            <div class="divide40"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12" style="text-align: center;">
+                        <h3>No High Pressure Salespeople Here</h3> 
+                        <h4>
+                            We’ll let our customers do the selling for us…
+                        </h4>
+
+                        <img src="img/kedasi/members/ITConnection_LogoOnly.png" width="180">
+                        <img src="img/kedasi/members/delegate.png" width="180">
+                        <img src="img/kedasi/members/dewaweb.png" width="180">
+                        <img src="img/kedasi/members/Krafthaus_Logo.png" width="180">
+                        <img src="img/kedasi/members/penulisid.jpg" width="180">
+                        <img src="img/kedasi/members/sukawu.png" width="180">
+                        <img src="img/kedasi/members/Logocc.png" width="180">
+                        <img src="img/kedasi/members/artunos.jpg" width="180">
+                         <img src="img/kedasi/members/8e_hires.png" width="180">
+                         
+                         <img src="img/kedasi/members/LOGO berlibur.png" width="180">
+                         <img src="img/kedasi/members/LOGO CIE.png" height="120">
+
+                        <hr>
+                    </div>
+                </div>
+            </div>
+
+
+              <div class="divide40"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12" style="text-align: center;">
+                        <h3>As Seen In</h3> 
+                        <!-- <h4>
+                            Beautifully crafted, fully furnished Coworking & Office Suites
+                        </h4>
+                        <h4>for entrepreneurs, startups and businesses.</h4>
+                        <p><a href="#" class="btn btn-theme-bg">About Us</a></p><hr> -->
+                    </div>
+                </div>
+            </div>
+       
+
+                    <div class="divide80"></div>
+        <div class="container">
+            <div class="row">   
+                <div class="col-md-6 margin30">
+                    <div class="row">                       
+                        <div class="col-md-12">                       
+                            <div class="blog-wrap">
+                                <div class="blog-desc">
+                                    <h4><a href="#">We make it you live it, go for simple </a></h4>
+                                    <ul class="list-inline post-deatil">
+                                        <li><a href="#">John Example</a></li>
+                                        <li>Posted on 2 august 2014</li>
+                                        <li>Posted in <a href="#">Sports</a></li>
+                                    </ul>
+                                    <p>
+                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                                    </p> 
+                                    
+                                </div>
+                            </div><!--blog wrap-->
+                           
+                        </div>
+                    </div><!--row-->
+                </div>
+
+                 <div class="col-md-6 margin30">
+                    <div class="row">                       
+                        <div class="col-md-12">                       
+                            <div class="blog-wrap">
+                                <div class="blog-desc">
+                                    <h4><a href="#">We make it you live it, go for simple </a></h4>
+                                    <ul class="list-inline post-deatil">
+                                        <li><a href="#">John Example</a></li>
+                                        <li>Posted on 2 august 2014</li>
+                                        <li>Posted in <a href="#">Sports</a></li>
+                                    </ul>
+                                    <p>
+                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                                    </p> 
+                                    
+                                </div>
+                            </div><!--blog wrap-->
+                           
+                        </div>
+                    </div><!--row-->
+                </div>
+                
+            </div>
+        </div><!--blog container-->
+        
+<?php include('footer.php') ?>
